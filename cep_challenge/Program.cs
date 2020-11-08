74a67e5a79a9f75cadeb8661d1ce455f0f280517
ecoltri2 <edr.ccol@gmail.com>
2020-11-08 13:27:15 -0300

    saving output excel if a err raises

--- cep_challenge/Program.cs
+++ cep_challenge/Program.cs
@@ -8,13 +8,13 @@ namespace cep_challenge
         static void Main(string[] args)
         {
             Logger.LogInitialStep();
+            Model excel_model = new Model(args[0]);
+            Model CEP_informations_collected = new Model();
+            Crawler crawler = new Crawler();
+            Controller controller = new Controller();
             try
             {
-                Model excel_model = new Model(args[0]);
-                Model CEP_informations_collected = new Model();
                 CEP_informations_collected.Create_excel_header(ConfigurationManager.AppSettings["cep_excel_output_headers"].Split(';'));
-                Controller controller = new Controller();
-                Crawler crawler = new Crawler();
                 crawler.Go_to_correios();
                 controller.Process_CEPs(ref excel_model, ref CEP_informations_collected, ref crawler);
                 CEP_informations_collected.Save_excel(args[1]);
@@ -24,6 +24,7 @@ namespace cep_challenge
             catch(System.Exception err)
             {
                 Logger.LogErrorStep(err.ToString());
+                if(CEP_informations_collected != null) CEP_informations_collected.Save_excel(args[1]);
                 System.Environment.Exit(1);
             }
         }
